3b48805bb1cbb592f87db8d9df78afe3cf33f112
sandeep <sandeep.patel8@wipro.com>
2019-11-23 19:22:23 +0530

    msg

---
+++ src/main/java/com/example/sandeep/externalConfigTest/ExternalConfigTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.sandeep.externalConfigTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalConfigTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExternalConfigTestApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/sandeep/externalConfigTest/TestController.java
@@ -0,0 +1,19 @@
+package com.example.sandeep.externalConfigTest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Value("${region.name}")
+    String envName;
+
+    @GetMapping("/findServerName")
+    public String getServerName(){
+        System.out.println(envName);
+        return envName;
+    }
+}
